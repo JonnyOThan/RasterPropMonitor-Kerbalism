e6a1189a8b7b6663fac9f4a0bdb8edb967d3b8ee
Jon Cable <jonnyothan@gmail.com>
2020-01-21 20:18:42 -0800

    Initial commit Includes RPMKPartModule and corresponding cfg file to intercept RPM's builtin ELECOUTPUTSOLAR, ELECTOUTPUTGENERATOR, and ELECOUTPUTFUELCELL variables.  Depends on code change in RPM to prioritize plugin variables over builtins, and Kerbalism's code changes to provde the API.ResourceBrokers() method.

---
+++ src/Kerbalism.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RasterPropMonitor_Kerbalism
+{
+	static class Kerbalism
+	{
+		static Func<Vessel, string, List<KeyValuePair<string, double>>> resourceBrokersDel;
+		static readonly List<KeyValuePair<string, double>> emptyBrokerList = new List<KeyValuePair<string, double>>();
+
+		static Kerbalism()
+		{
+			if (AssemblyLoader.loadedAssemblies.Any(a => String.Equals(a.name, "Kerbalism", StringComparison.InvariantCultureIgnoreCase)))
+			{
+				MethodInfo resourceBrokersInfo = Type.GetType("KERBALISM.API").GetMethod("ResourceBrokers");
+				resourceBrokersDel = (Func<Vessel, string, List<KeyValuePair<string, double>>>)
+					Delegate.CreateDelegate(typeof(Func<Vessel, string, List<KeyValuePair<string, double>>>), resourceBrokersInfo);
+			}
+		}
+
+		public static List<KeyValuePair<string, double>> GetResourceBrokers(Vessel vessel)
+		{
+			return resourceBrokersDel == null ? emptyBrokerList : resourceBrokersDel.Invoke(vessel, "ElectricCharge");
+		}
+	}
+}
---
+++ src/RPMKPartModule.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RasterPropMonitor_Kerbalism
+{
+	class RPMKPartModule : PartModule
+	{
+		public override void OnStart(StartState state)
+		{
+			UnityEngine.Debug.Log("RPMKPartModule.Start");
+		}
+
+		public override void OnFixedUpdate()
+		{
+			base.OnFixedUpdate();
+
+			var brokers = Kerbalism.GetResourceBrokers(vessel);
+
+			m_outputSolar = 0.0f;
+			m_outputGenerator = 0.0f;
+			m_outputFuelCell = 0.0f;
+
+			for (int i = 0; i < brokers.Count; ++i)
+			{
+				if (brokers[i].Key.Contains("solar"))
+				{
+					m_outputSolar += brokers[i].Value;
+				}
+				else if (brokers[i].Key.Contains("cell"))
+				{
+					m_outputFuelCell += brokers[i].Value;
+				}
+				else if (brokers[i].Key.Contains("generator"))
+				{
+					m_outputGenerator += brokers[i].Value;
+				}
+			}
+		}
+
+		public object HandleVariable(string variableName)
+		{
+			switch (variableName)
+			{
+				case "ELECOUTPUTSOLAR": return m_outputSolar;
+				case "ELECOUTPUTGENERATOR": return m_outputGenerator;
+				case "ELECOUTPUTFUELCELL": return m_outputFuelCell;
+			}
+
+			return null;
+		}
+
+		double m_outputSolar;
+		double m_outputGenerator;
+		double m_outputFuelCell;
+	}
+}
